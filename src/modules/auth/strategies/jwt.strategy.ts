357cebbbb2020b81e2550aa5515c544402646da8
Mayank Kuhaad <mayankkuhaad@gmail.com>
2025-06-10 09:51:59 +0530

    Initial commit: Working version of NestJS TaskFlow app

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -13,7 +13,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('jwt.secret'),
+      // secretOrKey: configService.get('jwt.secret'),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
